1b0259d9217a4ce3ac9a904db66d93fd79dca9e5
Sathananthan Sabesan <36799589+sabesansathananthan@users.noreply.github.com>
2020-06-15 23:09:23 +0530

    add: MaterialUIComponents (#2)

--- src/Contact.js
+++ src/Contact.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
-import 
+import TextField from "@material-ui/core/TextField";
 
 export default class Contact extends Component {
   state = {
     name: "",
     message: "",
     email: "",
+    subject: "",
     sent: false,
     buttonText: "Send Message",
   };
 
+  resetForm = () => {
+    this.setState({
+      name: "",
+      message: "",
+      email: "",
+      subject: "",
+      buttonText: "Message Sent",
+    });
+  };
+
   formSubmit = (e) => {
     e.preventDefault();
-
     this.setState({
       buttonText: "...sending",
     });
@@ -22,6 +32,7 @@ export default class Contact extends Component {
       name: this.state.name,
       email: this.state.email,
       message: this.state.message,
+      subject: this.state.subject,
     };
 
     axios
@@ -36,49 +47,59 @@ export default class Contact extends Component {
 
   render() {
     return (
-        
-
-
-
       <form className="contact-form" onSubmit={(e) => this.formSubmit(e)}>
-        <label class="message" htmlFor="message-input">
-          Your Message
-        </label>
-        <textarea
-          onChange={(e) => this.setState({ message: e.target.value })}
-          name="message"
-          class="message-input"
-          type="text"
-          placeholder="Please write your message here"
+        <TextField
+          id="standard-multiline-flexible"
+          label="Message"
+          placeholder="Enter message"
+          variant="outlined"
+          multiline
+          rowsMax={4}
           value={this.state.message}
+          onChange={(e) => this.setState({ message: e.target.value })}
           required
         />
+        <br />
+        <br />
+        <br />
 
-        <label class="message-name" htmlFor="message-name">
-          Your Name
-        </label>
-        <input
-          onChange={(e) => this.setState({ name: e.target.value })}
-          name="name"
-          class="message-name"
-          type="text"
-          placeholder="Your Name"
+        <TextField
+          id="outlined-basic"
+          placeholder="Enter your name"
+          label="Name"
+          variant="outlined"
           value={this.state.name}
-        />
-
-        <label class="message-email" htmlFor="message-email">
-          Your Email
-        </label>
-        <input
-          onChange={(e) => this.setState({ email: e.target.value })}
-          name="email"
-          class="message-email"
-          type="email"
-          placeholder="your@email.com"
+          onChange={(e) => this.setState({ name: e.target.value })}
           required
-          value={this.state.email}
         />
+        <br />
+        <br />
+        <br />
 
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          placeholder="Enter email address"
+          variant="outlined"
+          value={this.state.email}
+          onChange={(e) => this.setState({ email: e.target.value })}
+          required
+        />
+        <br />
+        <br />
+        <br />
+        <TextField
+          id="outlined-basic"
+          placeholder="Enter Subject"
+          label="Subject"
+          variant="outlined"
+          value={this.state.subject}
+          onChange={(e) => this.setState({ subject: e.target.value })}
+          required
+        />
+        <br />
+        <br />
+        <br />
         <div className="button--container">
           <button type="submit" className="button button-primary">
             {this.state.buttonText}
