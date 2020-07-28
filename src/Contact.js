554031f12501740b8e7edabfd6ea4a9ca6b24bcc
sabesansathananthan <sabesan.16@cse.mrt.ac.lk>
2020-07-28 17:13:01 +0530

    add:url

--- src/Contact.js
+++ src/Contact.js
@@ -61,7 +61,7 @@ export default class Contact extends Component {
     };
 
     try {
-      await axios.post("https://formbackend.vercel.app/api/v1", data);
+      await axios.post("form-backend-56ve5j7hk.vercel.app", data);
       this.setState({ sent: true }, this.resetForm());
     } catch (error) {
       console.log(error);
